e77bf5eb900ab50b8a1f72d2810437f2901a0d5f
James-Hong-Jey <jameshong8387@gmail.com>
2023-10-04 11:38:37 +0800

    Add oled

--- MODS/MODS.srcs/sources_1/new/Top_Student.v
+++ MODS/MODS.srcs/sources_1/new/Top_Student.v
@@ -3,18 +3,37 @@
 //////////////////////////////////////////////////////////////////////////////////
 //
 //  FILL IN THE FOLLOWING INFORMATION:
-//  STUDENT A NAME: 
+//  STUDENT A NAME: James Hong Jey
 //  STUDENT B NAME:
 //  STUDENT C NAME: 
 //  STUDENT D NAME:  
 //
 //////////////////////////////////////////////////////////////////////////////////
 
+/** 
+ * usage:
+ * new_clock clk625M (6250000, clock, <output>);
+ */ 
+module new_clock (input [32:0] frequency, input clock, output reg SLOW_CLOCK);
+    reg [32:0] COUNT = 0;
+    // Equation: 1 / frequency / 2 / 10^-8 / 10
+    always @ (posedge clock) begin
+        COUNT <= (COUNT == 100000000 / frequency / 2 - 1) ? 0 : COUNT + 1;
+        SLOW_CLOCK <= (COUNT == 0) ? ~SLOW_CLOCK : SLOW_CLOCK;
+    end
+endmodule
 
 module Top_Student (
-    // Delete this comment and include Basys3 inputs and outputs here
+    input clock, 
+    input [15:0] sw,
+    input btnC,
+    output [7:0] JB
     );
 
-    // Delete this comment and write your codes and instantiations here
-
+    wire clk, frame_begin, sending_pixels, sample_pixel;
+    wire [12:0] pixel_index;
+    new_clock clk6p25m (6250000, clock, clk);
+    wire [15:0] oled_data = sw[4] == 1 ? 16'h07E0 : 16'hF800;
+    Oled_Display(clk, btnC, frame_begin, sending_pixels, sample_pixel, pixel_index, oled_data,
+                JB[0], JB[1], JB[3], JB[4], JB[5], JB[6], JB[7]);
 endmodule
