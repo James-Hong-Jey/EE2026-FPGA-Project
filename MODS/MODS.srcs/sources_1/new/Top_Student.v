4c8796cf16641b3b5d76e3ba624ddc26851b29f0
James-Hong-Jey <jameshong8387@gmail.com>
2023-10-06 11:09:29 +0800

    Add Constants, add xy and border module. Not Tested

--- MODS/MODS.srcs/sources_1/new/Top_Student.v
+++ MODS/MODS.srcs/sources_1/new/Top_Student.v
@@ -23,6 +23,23 @@ module new_clock (input [32:0] frequency, input clock, output reg SLOW_CLOCK);
     end
 endmodule
 
+module xy (input [12:0] pixel_index, output [6:0] x, y);
+    assign x = pixel_index % `WIDTH;
+    assign y = pixel_index / `WIDTH;
+endmodule
+
+module border (
+    input [15:0] main_col, bg_col,
+    input [1:0] thickness,
+    input [6:0] x, y, 
+    output [15] oled_data
+    );
+
+    // thickness && (x <= thickness - 1 || x >= `WIDTH - thickness || y <= thickness - 1 || y >= `HEIGHT - thickness)
+    assign oled_data = thickness 0 ? main_col : bg_col;
+
+endmodule
+
 module Top_Student (
     input clock, 
     input [15:0] sw,
@@ -40,7 +57,8 @@ module Top_Student (
     wire clk, frame_begin, sending_pixels, sample_pixel;
     wire [12:0] pixel_index;
     new_clock clk6p25m (6250000, clock, clk);
-    // wire [15:0] oled_data = sw[4] == 1 ? 16'h07E0 : 16'hF800;
+    // wire [15:0] oled_data = sw[4] == 1 ? 16'hF800 : 16'h07E0;
+    // wire [15:0] oled_data = sw[4] == 1 ? `RED : `GREEN;
     Oled_Display(clk, btnC, frame_begin, sending_pixels, sample_pixel, pixel_index, oled_data,
                 JB[0], JB[1], JB[3], JB[4], JB[5], JB[6], JB[7]);
 
@@ -59,10 +77,8 @@ module Top_Student (
     assign setmax_x = 0;
     assign setmax_y = 0;
 
-    // clk, rst, [11:0] xpos, [11:0] ypos, [3:0] zpos
-    // left, middle, right, new_event
-    // [11:0] value, setx, sety, setmax_x, setmax_y
-    // ps2_clk, ps2_data
+    // clk, rst, [11:0] xpos, [11:0] ypos, [3:0] zpos, left, middle, right, new_event
+    // [11:0] value, setx, sety, setmax_x, setmax_y, ps2_clk, ps2_data
     MouseCtl(clk100, btnC, xpos, ypos, zpos, left, middle, right, new_event, 
                     value, setx, sety, setmax_x, setmax_y, PS2Clk, PS2Data);
 
