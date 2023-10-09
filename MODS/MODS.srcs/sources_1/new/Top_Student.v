2a5053228f5f0c5d97e9e2ba3771079871a2bcf5
James-Hong-Jey <jameshong8387@gmail.com>
2023-10-09 11:26:18 +0800

    Fixed Task 3B

--- MODS/MODS.srcs/sources_1/new/Top_Student.v
+++ MODS/MODS.srcs/sources_1/new/Top_Student.v
@@ -9,6 +9,7 @@
 //  STUDENT D NAME:  
 //
 //////////////////////////////////////////////////////////////////////////////////
+`include "constants.vh"
 
 /** 
  * usage:
@@ -32,25 +33,25 @@ module border (
     input [15:0] main_col, bg_col,
     input [1:0] thickness,
     input [6:0] x, y, 
-    output [15] oled_data
+    output [15:0] oled_data
     );
 
     // thickness && (x <= thickness - 1 || x >= `WIDTH - thickness || y <= thickness - 1 || y >= `HEIGHT - thickness)
-    assign oled_data = thickness 0 ? main_col : bg_col;
+    assign oled_data = thickness ? main_col : bg_col;
 
 endmodule
 
 module Top_Student (
     input clock, 
     input [15:0] sw,
-    input [15:0] led,
     input [6:0] seg,
     input btnC, // used as reset
 
     inout PS2Clk,
     inout PS2Data,
 
-    output [7:0] JB
+    output [7:0] JB,
+    output [15:0] led
     );
 
     // 3.A
@@ -58,7 +59,7 @@ module Top_Student (
     wire [12:0] pixel_index;
     new_clock clk6p25m (6250000, clock, clk);
     // wire [15:0] oled_data = sw[4] == 1 ? 16'hF800 : 16'h07E0;
-    // wire [15:0] oled_data = sw[4] == 1 ? `RED : `GREEN;
+    wire [15:0] oled_data = sw[4] == 1 ? `RED : `GREEN;
     Oled_Display(clk, btnC, frame_begin, sending_pixels, sample_pixel, pixel_index, oled_data,
                 JB[0], JB[1], JB[3], JB[4], JB[5], JB[6], JB[7]);
 
@@ -68,7 +69,8 @@ module Top_Student (
     wire clk100;
     new_clock clk100m (100000000, clock, clk100); 
 
-    wire [11:0] xpos, ypos, zpos;
+    wire [11:0] xpos, ypos;
+    wire [3:0] zpos;
     wire left, middle, right, new_event;
     wire [11:0] value = 0;
     wire setx, sety, setmax_x, setmax_y;
@@ -81,16 +83,16 @@ module Top_Student (
     // [11:0] value, setx, sety, setmax_x, setmax_y, ps2_clk, ps2_data
     MouseCtl(clk100, btnC, xpos, ypos, zpos, left, middle, right, new_event, 
                     value, setx, sety, setmax_x, setmax_y, PS2Clk, PS2Data);
-
-    assign led[15] = left; 
-    assign led[14] = middle; 
-    assign led[13] = right; 
+                    
+    assign led[15] = left;
+    assign led[14] = middle;
+    assign led[13] = right;
 
     // 3.C
     // UNTESTED
 
-    wire [11:0] mouse_press_x, mouse_press_y;
-    paint paint1(clk100, new_event, btnC, xpos, ypos, pixel_index, 
-        led, left, right, mouse_press_x, mouse_press_y, seg, oled_data);
+    // wire [11:0] mouse_press_x, mouse_press_y;
+    // paint paint1(clk100, new_event, btnC, xpos, ypos, pixel_index, 
+        // led, left, right, mouse_press_x, mouse_press_y, seg, oled_data);
 
 endmodule
